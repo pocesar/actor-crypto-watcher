ca0777b27ac43c27dd436b65247144a6cafe8b9b
Paulo Cesar <email@pocesar.e4ward.com>
2021-03-08 02:38:29 -0300

    first commit

---
+++ src/functions.ts
@@ -0,0 +1,327 @@
+import * as Apify from 'apify';
+import * as vm from 'vm';
+import * as moment from 'moment';
+
+const { log } = Apify.utils;
+
+export type AddressType = 'BTC' | 'ETH' | 'NONE';
+
+export interface Transaction {
+    tx: string;
+    block: string;
+    date: string;
+    from: Array<string | { value: number, address: string }>;
+    to: Array<string | { value: number, address: string }>;
+    amount: string;
+    fee: string;
+}
+
+export interface Output extends Omit<Transaction, 'to' | 'from' | 'date'> {
+    type: AddressType;
+    address: string;
+    balance: string;
+    from: string;
+    to: string;
+    tag: string | null;
+    date: string | null;
+    count: string | null;
+    scrapedAt: string;
+}
+
+export interface Info {
+    balance: string;
+    tag: string | null;
+    count: string | null;
+    transaction: Transaction | null;
+}
+
+export const XPATHS = {
+    ETH: {
+        BALANCE: '//div[@id="ContentPlaceHolder1_divSummary"]//div[contains(@class, "row")][contains(., "Balance:")]/div[2]',
+        TAG: '//div[@id="ContentPlaceHolder1_divSummary"]//div[contains(@class, "card-header")]//span[contains(@class, "u-label--secondary")]',
+        COUNT: '//div[@id="transactions"]//a[starts-with(@href, "/txs?a")]',
+        LAST_PAGE: '//a[@class="page-link" and contains(., "Last")]',
+        NO_ENTRIES: '//div[text()="There are no matching entries"]',
+    },
+};
+
+export interface Schema {
+    addresses: string[];
+    proxy?: any;
+    minDate?: string;
+    maxDate?: string;
+    maxTransactions?: number;
+    webhook?: string;
+    customData?: Record<string, any>;
+}
+
+/**
+ * Clean non number information from input
+ */
+export const cleanFloat = (number: number | string) => {
+    return `${number}`.replace(/[^\d.]+/g, '');
+};
+
+export const detectType = (address: string): AddressType => {
+    if (address?.startsWith('0x')) {
+        return 'ETH';
+    }
+
+    if (address?.match(/^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$/)) {
+        return 'BTC';
+    }
+
+    return 'NONE';
+};
+
+type PARAMS<T, CUSTOMDATA = any> = T & {
+    Apify: typeof Apify;
+    customData: CUSTOMDATA;
+    request: Apify.Request;
+};
+
+/**
+ * Compile a IO function for mapping, filtering and outputing items.
+ * Can be used as a no-op for interaction-only (void) functions on `output`.
+ * Data can be mapped and filtered twice.
+ *
+ * Provided base map and filter functions is for preparing the object for the
+ * actual extend function, it will receive both objects, `data` as the "raw" one
+ * and "item" as the processed one.
+ *
+ * Always return a passthrough function if no outputFunction provided on the
+ * selected key.
+ */
+export const extendFunction = async <RAW, INPUT extends Record<string, any>, MAPPED, HELPERS extends Record<string, any>>({
+    key,
+    output,
+    filter,
+    map,
+    input,
+    helpers,
+}: {
+    key: string,
+    map?: (data: RAW, params: PARAMS<HELPERS>) => Promise<MAPPED | MAPPED[]>,
+    output?: (data: MAPPED, params: PARAMS<HELPERS>) => Promise<void>,
+    filter?: (obj: { data: RAW, item: MAPPED }, params: PARAMS<HELPERS>) => Promise<boolean>,
+    input: INPUT,
+    helpers: HELPERS,
+}) => {
+    const base = {
+        ...helpers,
+        Apify,
+        customData: input.customData || {},
+    } as PARAMS<HELPERS>;
+
+    const evaledFn = (() => {
+        // need to keep the same signature for no-op
+        if (typeof input[key] !== 'string' || input[key].trim() === '') {
+            return new vm.Script('({ item }) => item');
+        }
+
+        try {
+            return new vm.Script(input[key], {
+                lineOffset: 0,
+                produceCachedData: false,
+                displayErrors: true,
+                filename: `${key}.js`,
+            });
+        } catch (e) {
+            throw new Error(`"${key}" parameter must be a function`);
+        }
+    })();
+
+    /**
+     * Returning arrays from wrapper function split them accordingly.
+     * Normalize to an array output, even for 1 item.
+     */
+    const splitMap = async (value: any, args: any) => {
+        const mapped = map ? await map(value, args) : value;
+
+        if (!Array.isArray(mapped)) {
+            return [mapped];
+        }
+
+        return mapped;
+    };
+
+    return async <T extends Record<string, any>>(data: RAW, args: T) => {
+        const merged = { ...base, ...args };
+
+        for (const item of await splitMap(data, merged)) {
+            if (filter && !(await filter({ data, item }, merged))) {
+                continue; // eslint-disable-line no-continue
+            }
+
+            const result = await (evaledFn.runInThisContext()({
+                ...merged,
+                data,
+                item,
+            }));
+
+            for (const out of (Array.isArray(result) ? result : [result])) {
+                if (output) {
+                    if (out !== null) {
+                        await output(out, merged);
+                    }
+                    // skip output
+                }
+            }
+        }
+    };
+};
+
+/**
+ * Do a generic check when using Apify Proxy
+ */
+export const proxyConfiguration = async ({
+    proxyConfig,
+    required = true,
+    force = Apify.isAtHome(),
+    blacklist = ['GOOGLESERP'],
+    hint = [],
+}: {
+    proxyConfig: any,
+    required?: boolean,
+    force?: boolean,
+    blacklist?: string[],
+    hint?: string[],
+}) => {
+    const configuration = await Apify.createProxyConfiguration(proxyConfig);
+
+    // this works for custom proxyUrls
+    if (required) {
+        if (!configuration || (!configuration.usesApifyProxy && !configuration.proxyUrls?.length) || !configuration.newUrl()) {
+            throw new Error(`\n=======\nYou're required to provide a valid proxy configuration\n\n=======`);
+        }
+    }
+
+    // check when running on the platform by default
+    if (force) {
+        // only when actually using Apify proxy it needs to be checked for the groups
+        if (configuration?.usesApifyProxy) {
+            if (blacklist.some((blacklisted) => configuration.groups?.includes(blacklisted))) {
+                throw new Error(`\n=======\nThese proxy groups cannot be used in this actor. Choose other group or contact support@apify.com to give you proxy trial:\n\n*  ${blacklist.join('\n*  ')}\n\n=======`);
+            }
+
+            // specific non-automatic proxy groups like RESIDENTIAL, not an error, just a hint
+            if (hint.length && !hint.some((group) => configuration.groups?.includes(group))) {
+                Apify.utils.log.info(`\n=======\nYou can pick specific proxy groups for better experience:\n\n*  ${hint.join('\n*  ')}\n\n=======`);
+            }
+        }
+    }
+
+    return configuration as Apify.ProxyConfiguration | undefined;
+};
+
+export const fixDate = (date: string | number) => {
+    const matches = `${date}`.match(/(\d{1,2}):(\d{1,2}):(\d{1,2})/);
+
+    if (!matches) {
+        return new Date(+date * 1000).toISOString();
+    }
+
+    const [matched, hour, minute, second] = matches;
+
+    return `${date}`.replace(matched, `${hour.padStart(2, '0')}:${minute.padStart(2, '0')}:${second.padStart(2, '0')}`);
+};
+
+export interface MinMax {
+    min?: number | string;
+    max?: number | string;
+}
+
+const parseTimeUnit = (value: any) => {
+    if (!value) {
+        return null;
+    }
+
+    if (value === 'today' || value === 'yesterday') {
+        return (value === 'today' ? moment() : moment().subtract(1, 'day')).startOf('day');
+    }
+
+    const [, number, unit] = `${value}`.match(/^(\d+) (minute|second|day|hour|month|year|week)s?$/i) || [];
+
+    if (+number && unit) {
+        return moment().subtract(+number, unit as any);
+    }
+
+    return moment(value);
+};
+
+/**
+ * Generate a function that can check date intervals depending on the input
+ */
+export const minMaxDates = ({ min, max }: MinMax) => {
+    const minDate = parseTimeUnit(min);
+    const maxDate = parseTimeUnit(max);
+
+    if (minDate && maxDate && maxDate.diff(minDate) < 0) {
+        throw new Error(`Minimum date ${minDate.toString()} needs to be less than max date ${maxDate.toString()}`);
+    }
+
+    return {
+        /**
+         * cloned min date, if set
+         */
+        get minDate() {
+            return minDate?.clone();
+        },
+        /**
+         * cloned max date, if set
+         */
+        get maxDate() {
+            return maxDate?.clone();
+        },
+        /**
+         * compare the given date/timestamp to the time interval
+         */
+        compare(time: string | number) {
+            const base = moment(time);
+            return (minDate ? minDate.diff(base) <= 0 : true) && (maxDate ? maxDate.diff(base) >= 0 : true);
+        },
+    };
+};
+
+/**
+ * Get transaction list
+ */
+export const createTxAddPage = (requestQueue: Apify.RequestQueue) => async (request: Apify.Request, newBase?: any, pageNumber?: number) => {
+    const { address, label, base, page } = request.userData;
+    pageNumber = pageNumber || ((page || 0) + 1);
+
+    return requestQueue.addRequest({
+        url: label === 'BTC'
+            ? `https://www.blockchain.com/btc/address/${address}?page=${pageNumber}`
+            : `https://etherscan.io/txs?a=${address}&p=${pageNumber}`,
+        userData: {
+            label,
+            address,
+            base: newBase || base,
+            main: false,
+            page: pageNumber,
+        },
+    });
+};
+
+/**
+ * Get wallet balances
+ */
+export const createAddWallet = (requestQueue: Apify.RequestQueue) => async (address: string) => {
+    const label = detectType(address);
+
+    if (label === 'NONE') {
+        log.warning(`Address is invalid: ${address}`);
+        return;
+    }
+
+    return requestQueue.addRequest({
+        url: label === 'BTC'
+            ? `https://www.blockchain.com/btc/address/${address}`
+            : `https://etherscan.io/address/${address}`,
+        userData: {
+            label,
+            address,
+        },
+    });
+};
---
+++ src/main.ts
@@ -0,0 +1,346 @@
+import * as Apify from 'apify';
+import * as _ from 'lodash';
+import * as moment from 'moment';
+import {
+    detectType,
+    Schema,
+    Output,
+    cleanFloat,
+    Transaction,
+    Info,
+    proxyConfiguration,
+    extendFunction,
+    XPATHS,
+    minMaxDates,
+    createTxAddPage,
+    createAddWallet,
+    fixDate,
+} from './functions';
+
+const { log } = Apify.utils;
+
+Apify.main(async () => {
+    const input: Schema = (await Apify.getInput()) as any;
+
+    if (!input || !input?.addresses?.length) {
+        throw new Error('Missing "addresses" array parameter');
+    }
+
+    const {
+        maxTransactions = 10,
+    } = input;
+
+    const requests = input.addresses.map<Apify.RequestOptions | undefined>((address) => {
+        const label = detectType(address);
+
+        if (label === 'NONE') {
+            log.warning(`Address is invalid: ${address}`);
+            return;
+        }
+
+        return {
+            url: label === 'BTC'
+                ? `https://www.blockchain.com/btc/address/${address}`
+                : `https://etherscan.io/address/${address}`,
+            userData: {
+                label,
+                address,
+            },
+        };
+    }).filter(s => s) as Apify.RequestOptions[];
+
+    const requestQueue = await Apify.openRequestQueue();
+
+    const addWallet = createAddWallet(requestQueue);
+    const addTxPage = createTxAddPage(requestQueue);
+
+    const addresses = new Map<string, number>((await Apify.getValue('ADDRESSES') as any[]) || []);
+    const txs = new Set((await Apify.getValue('TXS') as any[]) || []);
+
+    const persistState = async () => {
+        await Apify.setValue('ADDRESSES', [...addresses]);
+        await Apify.setValue('txs', [...txs]);
+    };
+
+    Apify.events.on('migrating', persistState);
+
+    for (const request of requests) {
+        await requestQueue.addRequest(request);
+    }
+
+    const checkDate = minMaxDates({
+        min: input.minDate,
+        max: input.maxDate,
+    });
+
+    const extendOutputFunction = await extendFunction<Info, Schema, Output, any>({
+        map: async (data, { request }) => {
+            const { address, label } = request.userData;
+            const { transaction } = data;
+
+            const base = {
+                type: label,
+                address,
+                tag: data.tag ?? null,
+                balance: cleanFloat(data.balance),
+                count: data.count ?? null,
+                scrapedAt: new Date().toISOString(),
+            } as Output;
+
+            return {
+                ...base,
+                amount: transaction ? cleanFloat(transaction.amount) : null,
+                from: transaction
+                    ? (data.tag === transaction.from[0] ? [address] : transaction.from)
+                    : [],
+                to: transaction
+                    ? data.tag === transaction.to[0] ? [address] : transaction.to
+                    : [],
+                block: transaction?.block ?? null,
+                tx: transaction?.tx ?? null,
+                date: transaction ? fixDate(transaction.date) : null,
+                fee: transaction ? cleanFloat(transaction.fee) : null,
+            };
+        },
+        filter: async ({ item }) => {
+            return (maxTransactions ? (addresses.get(item.address) || 0) < maxTransactions : true)
+                && (item.date ? checkDate.compare(item.date) : true)
+                && !txs.has(`${item.tx}${item.to}${item.from}${item.amount}`);
+        },
+        output: async (item) => {
+            addresses.set(item.address, (addresses.get(item.address) || 0) + 1);
+            txs.add(`${item.tx}${item.to}${item.from}`);
+            await Apify.pushData(item);
+        },
+        key: 'extendOutputFunction',
+        input,
+        helpers: {
+            _,
+            moment,
+            XPATHS,
+            checkDate,
+        },
+    });
+
+    const proxyConfig = await proxyConfiguration({
+        proxyConfig: input.proxy,
+    });
+
+    const extendScraperFunction = await extendFunction({
+        output: async () => { }, // eslint-disable-line
+        key: 'extendScraperFunction',
+        input,
+        helpers: {
+            _,
+            moment,
+            requestQueue,
+            detectType,
+            extendOutputFunction,
+            XPATHS,
+            checkDate,
+            addTxPage,
+            addWallet,
+        },
+    });
+
+    await extendScraperFunction(undefined, {
+        label: 'SETUP',
+    });
+
+    const crawler = new Apify.PuppeteerCrawler({
+        proxyConfiguration: proxyConfig,
+        requestQueue,
+        useSessionPool: true,
+        maxConcurrency: 10,
+        maxRequestRetries: 3,
+        preNavigationHooks: [async ({ page }, gotoOptions) => {
+            await Apify.utils.puppeteer.blockRequests(page);
+
+            gotoOptions.timeout = 30000;
+            gotoOptions.waitUntil = 'domcontentloaded';
+        }],
+        handlePageFunction: async ({ page, request }) => {
+            const { address, label, base } = request.userData;
+            let hasNextPage = true;
+
+            log.info(`Scraping ${label} address ${address}${
+                request.userData.page ? `, transaction page ${request.userData.page}` : ' balances'
+            }`);
+
+            if (label === 'ETH') {
+                if ((await page.$x(XPATHS.ETH.NO_ENTRIES)).length) {
+                    log.warning(`No transactions on ${address}`);
+                    await extendOutputFunction({
+                        balance: '0',
+                        count: '0',
+                        tag: null,
+                        transaction: null,
+                    }, {
+                        page,
+                        request,
+                    });
+                    return;
+                }
+
+                const [balance, tag, count] = await Promise.all([
+                    page.$x(XPATHS.ETH.BALANCE),
+                    page.$x(XPATHS.ETH.TAG),
+                    page.$x(XPATHS.ETH.COUNT),
+                ]);
+
+                if (!base) {
+                    const txCount = (await count?.[0].evaluate((s) => s.innerText.trim())) || null;
+
+                    await addTxPage(request, {
+                        balance: (await balance?.[0].evaluate((s) => s.innerText.trim())) || null,
+                        tag: (await tag?.[0].evaluate((s) => s.innerText.trim())) || null,
+                        count: txCount,
+                    }, 1);
+                } else {
+                    await page.waitForSelector('#paywall_mask table tbody tr');
+
+                    const transactions = await page.evaluate((): Transaction[] => {
+                        const trs = [...document.querySelectorAll<HTMLTableRowElement>('#paywall_mask table tbody tr')];
+                        const text = (tds: NodeListOf<HTMLTableCellElement>) => (
+                            index: number,
+                            getter: (td: HTMLTableCellElement) => any = (td) => td.innerText.trim(),
+                        ) => getter(tds?.[index]) || null;
+                        const parseTag = (selector: string) => (td: HTMLTableCellElement) => {
+                            const split = (
+                                td.querySelector<HTMLSpanElement>(selector)?.dataset?.originalTitle
+                                ?? td.querySelector<HTMLSpanElement>(selector)?.title
+                                ?? ''
+                            ).split(/(\(|\))/);
+
+                            if (split.length === 1) {
+                                return split[0].trim();
+                            }
+
+                            return split[2].trim();
+                        };
+                        const parseFrom = parseTag('a[data-original-title],span.text-truncate');
+                        const parseTo = parseTag('span > span');
+
+                        return trs.map((tr) => {
+                            const tds = text(tr.querySelectorAll('td'));
+
+                            return {
+                                tx: tds(1),
+                                block: tds(2),
+                                date: tds(4, (td) => td.querySelector('span')?.dataset?.originalTitle ?? null),
+                                from: [tds(5, parseFrom)],
+                                to: [tds(7, parseTo)],
+                                amount: tds(8),
+                                fee: tds(9),
+                            };
+                        });
+                    });
+
+                    const [lastPage] = await page.$x(XPATHS.ETH.LAST_PAGE);
+
+                    if (lastPage) {
+                        hasNextPage = await lastPage.evaluate((el) => (+(new URL(el?.href)?.searchParams?.get('p')) ?? 0))
+                            < (request.userData.page || 0) + 1;
+                    }
+
+                    for (const transaction of transactions) {
+                        await extendOutputFunction({
+                            ...base,
+                            transaction,
+                        }, {
+                            page,
+                            request,
+                        });
+                    }
+
+                    await extendScraperFunction(transactions, {
+                        request,
+                        page,
+                        label: 'TRANSACTIONS',
+                    });
+                }
+            } else if (label === 'BTC') {
+                await page.waitForSelector('#__NEXT_DATA__');
+
+                const json = _.get(await page.$eval('#__NEXT_DATA__', (s) => JSON.parse(s.innerHTML)), 'props.initialProps.pageProps');
+
+                if (!base) {
+                    await addTxPage(request, {
+                        balance: json.addressBalance.confirmed,
+                        tag: null,
+                        count: json.addressBalance.txs,
+                    }, 1);
+                } else {
+                    hasNextPage = (json?.addressTransactions?.length ?? 0) === (json?.pageSize ?? Infinity);
+
+                    for (const tx of json.addressTransactions) {
+                        await extendOutputFunction({
+                            ...base,
+                            transaction: {
+                                date: `${tx.time}`,
+                                fee: tx.fee,
+                                block: tx.block.height,
+                                amount: tx.inputs.reduce((out: number, { value }: any) => {
+                                    return out + value;
+                                }, 0),
+                                tx: tx.txid,
+                                from: tx.inputs.map((i: any) => ({ address: i.address, value: i.value })),
+                                to: tx.outputs.map((i: any) => ({ address: i.address, value: i.value })),
+                            },
+                        }, {
+                            page,
+                            request,
+                        });
+                    }
+
+                    await extendScraperFunction(json.addressTransactions, {
+                        request,
+                        page,
+                        label: 'TRANSACTIONS',
+                    });
+                }
+            }
+
+            if (base && hasNextPage) {
+                if ((addresses.get(address) || 0) < maxTransactions) {
+                    await addTxPage(request);
+                }
+            }
+
+            await extendScraperFunction(undefined, {
+                request,
+                page,
+                label: 'HANDLE',
+            });
+        },
+        handleFailedRequestFunction: async ({ request }) => {
+            const { address, label } = request.userData;
+
+            log.warning(`${label} address ${address} failed ${request.retryCount} times`);
+        },
+    });
+
+    await crawler.run();
+
+    if (input.webhook) {
+        await Apify.addWebhook({
+            eventTypes: ['ACTOR.RUN.SUCCEEDED'],
+            requestUrl: input.webhook,
+            idempotencyKey: Apify.getEnv().actorRunId!,
+            payloadTemplate: `{
+                "userId": {{userId}},
+                "customData": ${JSON.stringify(input.customData)},
+                "createdAt": {{createdAt}},
+                "eventType": {{eventType}},
+                "eventData": {{eventData}},
+                "resource": {{resource}}
+            }`,
+        });
+    }
+
+    await extendScraperFunction(undefined, {
+        label: 'FINISH',
+    });
+
+    log.info('Done');
+});
